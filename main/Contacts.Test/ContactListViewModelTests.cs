4c043518ea31dccf511a277ff14b7b8fe1b52c3c
Harish Babu <harish_babu@outlook.com>
2013-02-16 09:23:01 +0000

    ContactListViewModel file added

---
+++ main/Contacts.Test/ContactListViewModelTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Contacts.ViewModel;
+using NUnit.Framework;
+
+namespace Contacts.Test
+{
+    [TestFixture]
+    public class ContactListViewModelTests
+    {
+        [Test]
+        public void Test_ContactListViewModel_Implements_INotifyPropertyChanged()
+        {
+            ContactListViewModel viewModel = new ContactListViewModel();
+            Assert.IsInstanceOf<INotifyPropertyChanged>(viewModel);
+        }
+
+        [Test]
+        public void Test_ContactNames_IList_Raises_OnPropertyChanged_Event()
+        {
+            bool handled = false;
+            ContactListViewModel viewModel = new ContactListViewModel();
+            viewModel.PropertyChanged += (s, e) => { handled = true; };
+            viewModel.ContactNames = new List<string>();
+            Assert.IsTrue(handled);
+        }
+    }
+}
